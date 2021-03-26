a4604998dde9f70ebdafcfe1dff01c5a76a732e5
eunjun <iws1605@naver.com>
2021-03-26 23:53:08 +0900

    회원 관리 웹 애플리케이션 준비

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -1,5 +1,6 @@
 package hello.servlet;
 
+import hello.servlet.domain.member.MemberRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
